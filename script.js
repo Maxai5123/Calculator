b75abe0787ab6ef79fafa285c0c007a65c3b8b25
Maxim Borbashov <maxim.borbashov@gmail.com>
2025-11-17 18:27:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value += `${operator}`;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    display.value = eval(display.value);
+}
